efda52cfff5e26a5f0bc6720f44e9b4fb1d55be6
justinsta624 <justinsta624@gmail.com>
2024-01-22 23:02:11 -0500

    debug errors in seeds file
    debug errors in seeds file

--- seeds/index.js
+++ seeds/index.js
@@ -7,12 +7,12 @@ const seedUsers = require('./seedUsers.js');
 const seedAll = async () => {
     await sequelize.sync({ force: true });
     console.log('\n----- DATABASE SYNCED -----\n');
+    await seedUsers();
+    console.log('\n----- USERS SEEDED -----\n');
     await seedBlogs();
     console.log('\n----- BLOGS SEEDED -----\n');
     await seedComments();
     console.log('\n----- COMMENTS SEEDED -----\n');
-    await seedUsers();
-    console.log('\n----- USERS SEEDED -----\n');
 
     process.exit(0);
 };
