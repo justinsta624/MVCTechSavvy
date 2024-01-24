b75003beb87cdaa4c7b1bfd417b915f54fa6f514
justinsta624 <justinsta624@gmail.com>
2024-01-23 20:06:57 -0500

    fix errors in seedusers.js
    fix errors in seedusers.js

--- seeds/seedUsers.js
+++ seeds/seedUsers.js
@@ -2,38 +2,37 @@ const { User } = require('../models');
 
 const userData = [
     {
-        name: 'Hanbyeol(Justin)Lee',
+        username: 'Hanbyeol(Justin)Lee',
         email: 'justinsta624@gmail.com',
         password: 'justinsta624@gmail.com',
         active_ind: '1'
     },
     {
-        name: 'Jude Bellingham',
+        username: 'Jude Bellingham',
         email: 'Jude.Bellingham@gmail.com',
         password: 'Jude.Bellingham@gmail.com',
         active_ind: '1'
     },
     {
-        name: 'Erling Haaland',
+        username: 'Erling Haaland',
         email: 'Erling.Haaland@gmail.com',
         password: 'Erling.Haaland@gmail.com',
         active_ind: '1'
     },
     {
-        name: 'Kylian Mbappe',
+        username: 'Kylian Mbappe',
         email: 'Kylian.Mbappe@gmail.com',
         password: 'Kylian.Mbappe@gmail.com',
         active_ind: '1'
     },
     {
-        name: 'Heungmin Son',
+        username: 'Heungmin Son',
         email: 'Heungmin.Son@gmail.com',
         password: 'Heungmin.Son@gmail.com',
         active_ind: '1'
     },
     {
-        name: 'Inactive User',
-        username: 'inactive',
+        username: 'Inactive User',
         email: 'Inactive.User@gmail.com',
         password: 'Inactive.User@gmail.com',
         active_ind: '0'
