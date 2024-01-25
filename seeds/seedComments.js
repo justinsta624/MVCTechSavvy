69f29c1882fbe6c49a64b363c3bc7f3840143406
justinsta624 <justinsta624@gmail.com>
2024-01-24 20:22:22 -0500

    re-organize the seeds.js
    added id to each section

--- seeds/seedComments.js
+++ seeds/seedComments.js
@@ -2,6 +2,7 @@ const { Comment } = require('../models');
 
 const commentData = [
     {
+        comment_id: '1',
         comment_content: 'Great post! I learned a lot.',
         comment_date: '2024-01-09',
         active_ind: '1',
@@ -9,6 +10,7 @@ const commentData = [
         post_id: '1',
     },
     {
+        comment_id: '2',
         comment_content: 'Thanks for sharing this information.',
         comment_date: '2024-01-09',
         active_ind: '1',
@@ -16,6 +18,7 @@ const commentData = [
         post_id: '1',
     },
     {
+        comment_id: '3',
         comment_content: 'I have a question about your post, please check your DM',
         comment_date: '2024-01-09',
         active_ind: '1',
@@ -23,6 +26,7 @@ const commentData = [
         post_id: '1',
     },
     {
+        comment_id: '4',
         comment_content: 'Do you have more examples to share?',
         comment_date: '2024-01-10',
         active_ind: '1',
@@ -30,6 +34,7 @@ const commentData = [
         post_id: '1',
     },
     {
+        comment_id: '5',
         comment_content: 'This helped me understand the topic better.',
         comment_date: '2024-01-11',
         active_ind: '1',
@@ -37,6 +42,7 @@ const commentData = [
         post_id: '2',
     },
     {
+        comment_id: '6',
         comment_content: 'Nah, not really interested in this topic',
         comment_date: '2024-01-12',
         active_ind: '1',
@@ -44,6 +50,7 @@ const commentData = [
         post_id: '3',
     },
     {
+        comment_id: '7',
         comment_content: 'LOL, this is so interesting!',
         comment_date: '2024-01-12',
         active_ind: '1',
@@ -51,6 +58,7 @@ const commentData = [
         post_id: '4'
     },
     {
+        comment_id: '8',
         comment_content: 'I appreciate the detailed breakdown.',
         comment_date: '2024-01-13',
         active_ind: '1',
@@ -58,6 +66,7 @@ const commentData = [
         post_id: '5',
     },
     {
+        comment_id: '9',
         comment_content: 'This concept is fascinating.',
         comment_date: '2024-01-13',
         active_ind: '1',
@@ -65,6 +74,7 @@ const commentData = [
         post_id: '5',
     },
     {
+        comment_id: '10',
         comment_content: 'Can you explain more about it?',
         comment_date: '2024-01-14',
         active_ind: '1',
--- seeds/seedPosts.js
+++ seeds/seedPosts.js
@@ -2,6 +2,7 @@ const { Post } = require('../models');
 
 const postData = [
     {
+        post_id: '1',
         post_title: 'node.js',
         post_content: 'Node.js is an open-source, cross-platform JavaScript runtime environment that allows developers to build server-side and networked applications using JavaScript.',
         post_date: '2024-01-01',
@@ -9,6 +10,7 @@ const postData = [
         user_id: '1'
     },
     {
+        post_id: '2',
         post_title: 'MYSQL',
         post_content: 'MySQL is an open-source relational database management system (RDBMS) that uses Structured Query Language (SQL) for managing and manipulating data.',
         post_date: '2024-01-02',
@@ -16,6 +18,7 @@ const postData = [
         user_id: '2'
     },
     {
+        post_id: '3',
         post_title: 'RESTful APIs',
         post_content: 'RESTful APIs (Representational State Transfer APIs) are a set of architectural principles and conventions for designing and implementing web services.',
         post_date: '2024-01-03',
@@ -23,6 +26,7 @@ const postData = [
         user_id: '3'
     },
     {
+        post_id: '4',
         post_title: 'Bcrypt',
         post_content: 'Bcrypt is a password-hashing function designed to securely hash passwords.',
         post_date: '2024-01-04',
@@ -30,6 +34,7 @@ const postData = [
         user_id: '1'
     },
     {
+        post_id: '5',
         post_title: '(Inactive) Model View Controller (MVC)',
         post_content: 'Model-View-Controller (MVC) is a software architectural pattern commonly used in the design and development of user interfaces in software applications.',
         post_date: '2024-01-05',
@@ -37,6 +42,7 @@ const postData = [
         user_id: '2'
     },
     {
+        post_id: '6',
         post_title: 'DOTENV',
         post_content: 'DOTENV is a Node.js module that helps manage environment variables in applications by loading them from a file named .env.',
         post_date: '2024-01-06',
@@ -44,6 +50,7 @@ const postData = [
         user_id: '4'
     },
     {
+        post_id: '7',
         post_title: '(Inactive) Sequelize',
         post_content: 'Sequelize is a popular Object-Relational Mapping (ORM) library for Node.js which provides a powerful set of features to interact with relational databases using JavaScript.',
         post_date: '2024-01-07',
@@ -51,6 +58,7 @@ const postData = [
         user_id: '3'
     },
     {
+        post_id: '8',
         post_title: 'Handlebars.js',
         post_content: 'Handlebars.js is a popular JavaScript templating engine that simplifies the process of dynamically generating HTML by combining data with templates.',
         post_date: '2024-01-08',
